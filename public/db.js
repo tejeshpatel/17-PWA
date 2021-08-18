4791f6ac48224a1cc25aafc3dd46f864f172f4fa
tejeshpatel <tejeshpatel@live.com>
2021-08-17 19:14:46 -0500

    pushing to heroku

--- public/db.js
+++ public/db.js
@@ -26,3 +26,42 @@ request.onsuccess = ({target}) => {
 request.onerror = function(event) {
     console.log("Something went wrong", event.target.errorCode)
 }
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], 'readwrite')
+    const store = transaction.createObjectStore("pending")
+
+    store.add(record)
+}
+
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {        
+          return response.json();
+        })
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", checkDatabase);
--- server.js
+++ server.js
@@ -15,10 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/classDeployPWA", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  unUnifiedTopology: true
 });
+
 
 // routes
 app.use(require("./routes/api.js"));
